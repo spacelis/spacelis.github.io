a64e03699dd077bc7d9e27b848972aba1e6b3145
Wen Li <spacelis@gmail.com>
2016-10-27 23:13:37 +0100

    rebuilding site Thu, 27 Oct, 2016 23:13:37

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push
+git subtree push --prefix=public git@github.com:spacelis/spacelis.github.io master
